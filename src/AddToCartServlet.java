a09810cf2816521b05e16dbb013a82976f8a52fc
bhuvanc08 <bhuvanc@uci.edu>
2024-04-28 18:00:08 -0700

    Quantity fixed

--- src/AddToCartServlet.java
+++ src/AddToCartServlet.java
@@ -7,14 +7,16 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.HashMap;
 
-
 @WebServlet(name = "AddToCartServlet", urlPatterns = "/addToCart")
 public class AddToCartServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         // Extract movie details from request parameters
         String movieId = request.getParameter("movieId");
         String movieTitle = request.getParameter("movieTitle");
-        System.out.println("Received request to add movie to cart. Movie ID: " + movieId + ", Movie Title: " + movieTitle); // Debug statement
+        System.out.println(
+                "Received request to add movie to cart. Movie ID: " + movieId + ", Movie Title: " + movieTitle); // Debug
+                                                                                                                 // statement
 
         // Default quantity is 1 if not provided in the request
         int quantity = 1;
@@ -24,21 +26,26 @@ public class AddToCartServlet extends HttpServlet {
         }
         System.out.println("Quantity: " + quantity); // Debug statement
 
-        // Retrieve the user's shopping cart from the session or create a new cart if it doesn't exist
+        // Retrieve the user's shopping cart from the session or create a new cart if it
+        // doesn't exist
         Map<String, Integer> cart = (Map<String, Integer>) request.getSession().getAttribute("cart");
         if (cart == null) {
             cart = new HashMap<>();
             request.getSession().setAttribute("cart", cart);
         }
-
-        // Add or update the movie in the cart
-        cart.put(movieTitle, quantity);
+        if (cart.containsKey(movieTitle)) {
+            // If the movie already exists, update the quantity
+            int existingQuantity = cart.get(movieTitle);
+            cart.put(movieTitle, existingQuantity + quantity);
+        } else {
+            // If the movie doesn't exist, add it to the cart
+            cart.put(movieTitle, quantity);
+        }
         System.out.println("Movie added to cart. Cart contents: " + cart); // Debug statement
 
         // Send success response
         response.setContentType("application/json");
         response.getWriter().write("{\"success\": true}");
     }
-
 
 }
